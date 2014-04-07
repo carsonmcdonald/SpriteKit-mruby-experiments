ac097c36e21a29eaeef11a7f422fa22b63bbd1b8
Carson McDonald <carson@ioncannon.net>
2014-04-07 09:40:38 -0400

    Add scene in script

--- OSXExperiments/OSXExperiments/test.rb
+++ OSXExperiments/OSXExperiments/test.rb
@@ -1,7 +1,13 @@
 begin
     SKMR::show_debug = true
     
-    main_scene = SKMR::Scene.new
+    view_size = {:x=>800, :y=>600}
+    
+    main_scene = SKMR::Scene.new(view_size[:x], view_size[:y])
+    
+    main_scene.background_color = "#0033A0"
+    
+    SKMR::current_scene = main_scene
 rescue Exception => e
     puts e
 end
