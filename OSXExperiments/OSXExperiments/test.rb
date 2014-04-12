754878ac84e7a68759c8b9167cb4d793eb321f5b
Carson McDonald <carson@ioncannon.net>
2014-04-11 20:36:45 -0400

    Updated with new attributes

--- OSXExperiments/OSXExperiments/test.rb
+++ OSXExperiments/OSXExperiments/test.rb
@@ -7,6 +7,13 @@ begin
     
     main_scene.background_color = "#0033A0"
     
+    label = SKMR::Label.new
+    label.text = "Hello, World!"
+    label.font_size = 64
+    label.font_color = "#990099"
+    label.position = [view_size[:x]/2, view_size[:y]/2]
+    main_scene << label
+    
     SKMR::current_scene = main_scene
 rescue Exception => e
     puts e
